db876773503bc69892b48e11bdaed879d3110514
Kamila Bayandinova <k.bayandinova@gmail.com>
2025-02-23 16:48:41 +0600

    partners page changed

--- src/pages/Partners/FinancialPartners/FinancialPartners.tsx
+++ src/pages/Partners/FinancialPartners/FinancialPartners.tsx
@@ -17,7 +17,9 @@ const FinancialPartners = () => {
           <p className="mb-7 text-2xl md:text-3xl lg:text-4xl">{t('routes.financialPartners')}</p>
           <div className="flex justify-between flex-wrap">
               {document.data.body[1].items.map((item: any) => (
+                  <div  className="w-[430px] mb-4 bg-gray_F8 p-8 rounded-lg">
                   <img key={item.id} src={item.logo.url} alt={item.logo.alt} className="mb-4"/>
+                  </div>
               ))}
           </div>
       </div>
